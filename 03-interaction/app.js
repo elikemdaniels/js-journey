78989a25cd7d01b16d70749695280eaa6faf1a08
elikemdaniels <hello@elikemdaniels.com>
2023-12-12 05:38:57 +0000

    #

--- 03-interaction/app.js
+++ 03-interaction/app.js
@@ -5,3 +5,8 @@
 
 // Prompt
 const age = prompt('How old are you?', '');
+if (age > 17) {
+  alert(`You are ${age} years old, and you can drive.`);
+} else {
+  alert(`You are ${age} years old, and you CANNOT drive.`);
+}
