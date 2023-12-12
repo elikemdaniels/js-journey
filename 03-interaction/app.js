fd86621879eb97dc3b27a36b9a70fc27342e2e5f
elikemdaniels <hello@elikemdaniels.com>
2023-12-12 05:52:44 +0000

    #

--- 03-interaction/app.js
+++ 03-interaction/app.js
@@ -4,9 +4,13 @@
 // alert('Good morning');
 
 // Prompt
-const age = prompt('How old are you?', '');
-if (age > 17) {
-  alert(`You are ${age} years old, and you can drive.`);
-} else {
-  alert(`You are ${age} years old, and you CANNOT drive.`);
-}
+// const age = prompt('How old are you?', '');
+// if (age > 17) {
+//   alert(`You are ${age} years old, and you can drive.`);
+// } else {
+//   alert(`You are ${age} years old, and you CANNOT drive.`);
+// }
+
+// Confirm
+// let isBoss = confirm('Are you the boos?');
+// alert(isBoss);
