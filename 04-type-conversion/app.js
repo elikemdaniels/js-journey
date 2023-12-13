39c7f1e2f940af8dbfc790df4f83dad1ea282cdc
elikemdaniels <hello@elikemdaniels.com>
2023-12-13 04:50:46 +0000

    #

--- 04-type-conversion/app.js
+++ 04-type-conversion/app.js
@@ -5,5 +5,7 @@
 // console.log(typeof String(num));
 
 // Number Conversion
-const figure = '4500';
-console.log(typeof Number(figure));
+// const figure = '4500';
+// console.log(typeof Number(figure));
+
+// Boolean Conversion
