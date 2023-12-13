bba06129763d69a2acfa6bc14946ce6347ecf06d
elikemdaniels <hello@elikemdaniels.com>
2023-12-13 22:29:46 +0000

    #

--- 06-conditional-branching/app.js
+++ 06-conditional-branching/app.js
@@ -1,9 +1,12 @@
 // CONDITIONAL BRANCHING
 
-let year = prompt('In which year was ECMAScript-2015 specification published?');
+// let year = prompt('In which year was ECMAScript-2015 specification published?');
 
-if (year == 2015) {
-  alert('Correct!');
-} else {
-  alert('Wrong answer. Try again.');
-}
+// if (year == 2015) {
+//   alert('Correct!');
+// } else {
+//   alert('Wrong answer. Try again.');
+// }
+
+let accessAllowed;
+let age = prompt('How old are you?');
