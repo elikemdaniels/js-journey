38181961a68521c295ab74d62c0be151a369a4ac
elikemdaniels <hello@elikemdaniels.com>
2023-12-13 17:25:36 +0000

    Practised simple if statements with some comparisons

--- 06-conditional-branching/app.js
+++ 06-conditional-branching/app.js
@@ -1,1 +1,9 @@
 // CONDITIONAL BRANCHING
+
+let year = prompt('In which year was ECMAScript-2015 specification published?');
+
+if (year == 2015) {
+  alert('Correct!');
+} else {
+  alert('Wrong answer. Try again.');
+}
