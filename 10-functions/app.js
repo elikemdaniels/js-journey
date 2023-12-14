b05b5cba2b03756fa5fd76dacc8fc2fc6674a0ef
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 21:10:20 +0000

    #

--- 10-functions/app.js
+++ 10-functions/app.js
@@ -40,3 +40,40 @@
 // }
 
 // showMessage('How are you?');
+
+// Returning a Value
+// function numberAdder(num1, num2) {
+//   return num1 + num2;
+// }
+
+// let result = numberAdder(120, 300);
+// console.log(result);
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return confirm('Do you have permission?');
+//   }
+// }
+
+// let age = prompt('How old are you?', 18);
+
+// if (checkAge(age)) {
+//   alert('Permission granted.');
+// } else {
+//   alert('Permission denied.');
+// }
+
+// function checkAge(age) {
+//   return age > 18 ? true : confirm('Hello');
+// }
+
+// console.log(checkAge(20));
+
+// Function Expression
+let addNumbers = function (a, b) {
+  return a + b;
+};
+let mee = addNumbers;
+console.log(mee(2, 122));
