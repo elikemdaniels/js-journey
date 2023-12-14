059bbd4a9b3052858b922bc9ac59b2726262bc23
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 04:02:09 +0000

    #

--- 06-conditional-branching/app.js
+++ 06-conditional-branching/app.js
@@ -8,11 +8,13 @@
 //   alert('Wrong answer. Try again.');
 // }
 
-let age = prompt('How old are you?');
-age = Number(age);
+// let age = prompt('How old are you?');
+// age = Number(age);
 
 // if (age >= 18) {
 //   accessAllowed = alert(`You're ${age} years old. You can vote.`);
 // } else {
 //   accessAllowed = alert(`You're ${age} years old. You cannot vote.`);
 // }
+
+// let result = a + b < 4 ? 'Bellow' : 'Over';
