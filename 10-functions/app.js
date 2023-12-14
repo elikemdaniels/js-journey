d3e7790bec36a01f01ce80a974ddd26fae1e1e27
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 17:33:13 +0000

    Learnt about function declaration

--- 10-functions/app.js
+++ 10-functions/app.js
@@ -1,1 +1,14 @@
 // FUNCTIONS
+
+// Function Declaration
+// function showMessage() {
+//   console.log('Hello, JavaScript');
+// }
+
+// showMessage();
+
+// function showMessage(username) {
+//   console.log(`Hello, ${username}. You're welcome.`);
+// }
+
+// showMessage('elikem');
