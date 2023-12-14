6bc2f5ec966c33d56467cd4142b2d69f8a676b2a
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 21:28:47 +0000

    Converted function expression into arrow function

---
+++ 11-arrow-functions/app.js
@@ -0,0 +1,11 @@
+// ARROW FUNCTIONS
+
+// Function Expression Conversion to Arrow Function
+// let addNumbers = function (a, b) {
+//   return a + b;
+// };
+// let mee = addNumbers;
+// console.log(mee(2, 122));
+
+let addNumbers = (a, b) => a + b;
+console.log(addNumbers(100, 122));
