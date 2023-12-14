2830cc8f0ad09bb103b9a119c933736d474dd240
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 05:44:45 +0000

    #

--- 08-loops/app.js
+++ 08-loops/app.js
@@ -6,8 +6,20 @@
 // while (i < 5) console.log(i++);
 
 // Do while loop
-let index = 0;
-do {
-  console.log(index);
-  index++;
-} while (index < 30);
+// let index = 10;
+// do {
+//   console.log(index);
+//   index++;
+// } while (index < 30);
+
+// For loop
+// for (variable; condition; iterator) {
+//     code
+// }
+
+let foodItems = ['tomato', 'pepper', 'onion', 'garlic', 'carrot', 'lettuce'];
+let foodItem = 0;
+
+for (; foodItem < foodItems.length; foodItem++) {
+  console.log(foodItem);
+}
