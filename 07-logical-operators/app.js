c37e2cb8ef1f4786d826c0fc1c8496c5c98443c7
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 04:17:33 +0000

    Logical OR

--- 07-logical-operators/app.js
+++ 07-logical-operators/app.js
@@ -1,1 +1,14 @@
 // LOGICAL OPERATORS
+
+// Logical OR
+let hour = 19;
+// if (hour < 10 || hour > 17) {
+//   console.log('Office is closed.');
+// } else {
+//   console.log('We are open for business.');
+// }
+
+let message =
+  hour < 10 || hour > 17
+    ? console.log('Office is closed.')
+    : console.log('We are open for business.');
