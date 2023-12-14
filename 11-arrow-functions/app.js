e4b27855d809fb3716bfee168a0e417a8e72c1ea
elikemdaniels <hello@elikemdaniels.com>
2023-12-14 21:34:48 +0000

    #

--- 11-arrow-functions/app.js
+++ 11-arrow-functions/app.js
@@ -7,5 +7,8 @@
 // let mee = addNumbers;
 // console.log(mee(2, 122));
 
-let addNumbers = (a, b) => a + b;
-console.log(addNumbers(100, 122));
+// let addNumbers = (a, b) => a + b;
+// console.log(addNumbers(100, 122));
+
+let sayHi = () => console.log('Hello!');
+sayHi();
