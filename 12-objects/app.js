7aceec5b18da819e2f64927c1819149b77d0baed
elikemdaniels <hello@elikemdaniels.com>
2023-12-15 00:19:52 +0000

    Deleted a property from teacher object

--- 12-objects/app.js
+++ 12-objects/app.js
@@ -1,12 +1,15 @@
 // ONJECTS
 
 // Object constructor syntax
-let student = new Object();
-console.log(student);
+// let student = new Object();
+// console.log(student);
 
 // Object literal syntax
 let teacher = {
   name: 'Jeremy',
   class: 'Science',
+  isNew: false,
 };
-console.log(teacher.name);
+
+let deletedProperty = delete teacher.class;
+console.log(teacher);
