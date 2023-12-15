36dd2977841757299bae867226f4a01fe25efb15
elikemdaniels <hello@elikemdaniels.com>
2023-12-15 01:18:11 +0000

    #

--- 12-objects/app.js
+++ 12-objects/app.js
@@ -5,13 +5,26 @@
 // console.log(student);
 
 // Object literal syntax
-let teacher = {
-  name: 'Jeremy',
-  age: 35,
-  class: 'Science',
-  isNew: false,
-};
+// let teacher = {
+//   name: 'Jeremy',
+//   age: 35,
+//   class: 'Science',
+//   isNew: false,
+// };
 
-let key = prompt('What do you want to know about the teacher?', 'name');
+// let key = prompt('What do you want to know about the teacher?', 'name');
 
-alert(`The teacher's ${key} is ${teacher[key]}.`);
+// alert(`The teacher's ${key} is ${teacher[key]}.`);
+
+function makeUser(name, age, gender, country) {
+  return {
+    name,
+    age,
+    gender,
+    country,
+  };
+}
+
+let newUser = makeUser('Elikem', 30, 'Male', 'Ghana');
+userCountry = newUser.country;
+console.log('age' in newUser);
