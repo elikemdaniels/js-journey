dc49c8bfbb5ab47d7cf63fcbd934e1759543f698
elikemdaniels <hello@elikemdaniels.com>
2023-12-15 00:14:54 +0000

    #

--- 12-objects/app.js
+++ 12-objects/app.js
@@ -1,1 +1,12 @@
 // ONJECTS
+
+// Object constructor syntax
+let student = new Object();
+console.log(student);
+
+// Object literal syntax
+let teacher = {
+  name: 'Jeremy',
+  class: 'Science',
+};
+console.log(teacher.name);
