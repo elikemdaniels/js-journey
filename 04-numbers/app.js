8476218bacd0d2271682b2c62453209d3e9fdf0a
elikemdaniels <hello@elikemdaniels.com>
2023-12-20 04:50:31 +0000

    Rounding numbers

--- 04-numbers/app.js
+++ 04-numbers/app.js
@@ -1,5 +1,19 @@
 // NUMBERS
 
-let num = 255;
+// toString(base)
+// let num = 255;
+// console.log((67839406578907654 - 7656789).toString(36));
 
-console.log((67839406578907654 - 7656789).toString(36));
+// Rounding
+// console.log(Math.floor(3.1));
+// console.log(Math.ceil(3.1));
+// console.log(Math.round(3.1));
+// console.log(Math.round(3.6));
+// console.log(Math.round(3.5));
+// console.log(Math.trunc(3.1));
+
+let num = 2.3456789;
+
+console.log(Math.round(num * 100) / 100); // round to 2 decimals
+console.log(Math.round(num * 1000) / 1000); // round to 3 decimals
+console.log(Math.round(num * 10000) / 10000); // round to 4 decimals
