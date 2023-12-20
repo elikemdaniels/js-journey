2a2dc2a0aab69f35d848e544d1df4d825fff3875
elikemdaniels <hello@elikemdaniels.com>
2023-12-20 15:54:41 +0000

    #

--- 14-strings/app.js
+++ 14-strings/app.js
@@ -14,4 +14,33 @@ let str = 'elikem';
 //   console.log('Found it!');
 // }
 
-console.log(str[0].toUpperCase() + str.slice(1));
+// console.log(str[0].toUpperCase() + str.substring(1));
+
+// console.log(str[0].toUpperCase() + str.slice(1));
+
+/**
+ * Checks if a string contains spam terms.
+ *
+ * @param {string} inputString - The string to check for spam terms
+ * @returns {boolean} Whether the string contains spam terms
+//  */
+// function checkForSpam(inputString) {
+//   const spamTerms = ['viagra', 'xxx', 'pharmacy'];
+
+//   return spamTerms.some((term) => inputString.includes(term));
+// }
+
+// console.log(checkForSpam('buy viagra now')); // true
+
+// function checkForSpam(inputString) {
+//   let lowerCaseString = inputString.toLowerCase();
+
+//   return lowerCaseString.includes('viagra') || lowerCaseString.includes('xxx');
+// }
+
+// console.log(checkForSpam('buy Vwiagra now for XXXXXXXXX')); // true
+
+function truncateString(str, maxLength) {
+  return str.length > maxLength ? str.slice(0, maxLength - 1) + '...' : str;
+}
+console.log(truncateString('Jehunamabugatti', 12)); // 'He...'
