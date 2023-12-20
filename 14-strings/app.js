2f9ad19591d0ac0f4f2dcd6b72bae6db3f53e703
elikemdaniels <hello@elikemdaniels.com>
2023-12-20 11:58:39 +0000

    #

--- 14-strings/app.js
+++ 14-strings/app.js
@@ -1,22 +1,14 @@
-// OBJECTS
+// STRINGS
+let username = 'Widget with moo';
 
-/**
- * Creates a student object with properties like name, age, gender etc.
- * Calculates the number of properties in the student object using Object.keys.
- * Logs the number of properties to the console.
- */
-// const student = {
-//   name: 'Elikem',
-//   age: 30,
-//   gender: 'Male',
-//   country: 'Ghana',
-//   favColor: 'Blue',
-//   hobbies: ['Sports', 'Reading', 'Music'],
-//   isMarried: true,
-//   favNumbers: [1, 2, 3, 4, 5],
-//   sayHello() {
-//     console.log(`Hello, ${student.name}`);
-//   },
-// };
-// const studentLength = Object.keys(student).length;
-// console.log(studentLength);
+// console.log(username[0]);
+// console.log(username[username.length - 2]);
+// console.log(username.at(-2));
+
+// console.log(username.indexOf('Widget'));
+// console.log(username.indexOf('with'));
+// console.log(username.indexOf('moo'));
+
+if (username.includes('Widget')) {
+  console.log('Found it!');
+}
