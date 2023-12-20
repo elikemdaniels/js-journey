bbb3da790d461d5a5d1a71a8ed7a6e6b18885f1f
elikemdaniels <hello@elikemdaniels.com>
2023-12-20 03:49:53 +0000

    #

--- 12-objects/app.js
+++ 12-objects/app.js
@@ -5,18 +5,18 @@
  * Calculates the number of properties in the student object using Object.keys.
  * Logs the number of properties to the console.
  */
-const student = {
-  name: 'Elikem',
-  age: 30,
-  gender: 'Male',
-  country: 'Ghana',
-  favColor: 'Blue',
-  hobbies: ['Sports', 'Reading', 'Music'],
-  isMarried: true,
-  favNumbers: [1, 2, 3, 4, 5],
-  sayHello() {
-    console.log(`Hello, ${student.name}`);
-  },
-};
-const studentLength = Object.keys(student).length;
-console.log(studentLength);
+// const student = {
+//   name: 'Elikem',
+//   age: 30,
+//   gender: 'Male',
+//   country: 'Ghana',
+//   favColor: 'Blue',
+//   hobbies: ['Sports', 'Reading', 'Music'],
+//   isMarried: true,
+//   favNumbers: [1, 2, 3, 4, 5],
+//   sayHello() {
+//     console.log(`Hello, ${student.name}`);
+//   },
+// };
+// const studentLength = Object.keys(student).length;
+// console.log(studentLength);
