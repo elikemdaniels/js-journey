772c8163dcbe451a804595d05cf960a144e502a5
elikemdaniels <hello@elikemdaniels.com>
2023-12-20 10:50:54 +0000

    Generated random numbers

--- 04-numbers/app.js
+++ 04-numbers/app.js
@@ -12,10 +12,23 @@
 // console.log(Math.round(3.5));
 // console.log(Math.trunc(3.1));
 
-let num = 2.3456789;
+// let num = 2.3456789;
 
-console.log(Math.round(num * 100) / 100); // round to 2 decimals
-console.log(Math.round(num * 1000) / 1000); // round to 3 decimals
-console.log(Math.round(num * 10000) / 10000); // round to 4 decimals
+// console.log(Math.round(num * 100) / 100); // round to 2 decimals
+// console.log(Math.round(num * 1000) / 1000); // round to 3 decimals
+// console.log(Math.round(num * 10000) / 10000); // round to 4 decimals
 
-console.log(num.toFixed(2)); // round to 2 decimals, and returns a string value
+// console.log(num.toFixed(2)); // round to 2 decimals, and returns a string value
+
+// let num1 = 0.1;
+// let num2 = 0.2;
+
+// let result = num1 + num2;
+
+// console.log(Math.random().toString(36).slice(2));
+
+function random(min, max) {
+  return (Math.random() * (max - min + 1) + min).toString(30).slice(2);
+}
+
+console.log(random(1, 9));
