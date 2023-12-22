92c3072650e254ee04e332e6e4a1bce598b4d229
elikemdaniels <hello@elikemdaniels.com>
2023-12-22 04:12:21 +0000

    Array & array methods

---
+++ 15-arrays/app.js
@@ -0,0 +1,36 @@
+// ARRAYS
+
+let cars = ['BMW', 'Volvo', 'Saab', 'Ford'];
+
+console.log(cars);
+
+cars.pop();
+console.log(cars);
+
+cars.shift();
+console.log(cars);
+
+cars.unshift('Toyota');
+console.log(cars);
+
+cars.push('Tesla', 'Jaguar', 'Land Rover', 'Hyundai');
+console.log(cars);
+console.log(cars.length);
+
+let newCars = cars;
+
+console.log(newCars);
+
+newCars.pop();
+console.log(newCars);
+
+newCars.unshift('Peugeot', 'Mazda', 'Ford');
+console.log(newCars);
+console.log(cars);
+
+console.log((newCars[0] = 'BMW'));
+console.log(cars);
+
+// for (let car = 0; car < cars.length; car++) {
+//   console.log(cars[car]);
+// }
