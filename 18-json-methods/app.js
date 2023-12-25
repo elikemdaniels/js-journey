74ecb6750e27a74f234c5a7d405c9d51bb45f978
elikemdaniels <hello@elikemdaniels.com>
2023-12-25 21:36:46 +0000

    #

---
+++ 18-json-methods/app.js
@@ -0,0 +1,24 @@
+// JSON Methods
+
+let student = {
+  id: 1,
+  name: 'John',
+  age: 20,
+  grades: [9, 10, 10, 10, 9],
+  attendace: 120,
+  favoriteCar: {
+    model: 'BMW',
+    color: 'black',
+    year: 2018,
+  },
+};
+
+// console.log(student);
+// console.log(JSON.stringify(student));
+// console.log(JSON.parse(JSON.stringify(student)));
+
+// console.log(student.favoriteCar);
+// console.log(JSON.stringify(student, null, 4));
+// console.log(JSON.parse(JSON.stringify(student, ['id', 'name', 'age'])));
+
+);
