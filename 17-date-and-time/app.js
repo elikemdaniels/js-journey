1c44ebaa33c004bb6f018e8dff1f3506d15cf2f8
elikemdaniels <hello@elikemdaniels.com>
2023-12-25 21:06:56 +0000

    #

--- 17-date-and-time/app.js
+++ 17-date-and-time/app.js
@@ -1,4 +1,10 @@
 // DATE & TIME
 
 let currentDate = new Date();
+
+currentDate = currentDate.setUTCYear(currentDate.getUTCFullYear());
+// console.log(
+//   `The current time is ${currentDate.getUTCHours()}:${currentDate.getUTCMinutes()}:${currentDate.getUTCSeconds()}`
+// );
+
 console.log(currentDate);
