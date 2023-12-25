d6a1dc3b6c5152d940575b6d5bd0d7b061d67574
elikemdaniels <hello@elikemdaniels.com>
2023-12-25 16:39:51 +0000

    #

--- 16-map-and-set/app.js
+++ 16-map-and-set/app.js
@@ -1,1 +1,29 @@
 // MAP & SET
+let students = [
+  {
+    id: 1,
+    name: 'John',
+    age: 20,
+    grades: [9, 10, 10, 10, 9],
+    attendace: 120,
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    age: 22,
+    grades: [10, 10, 9, 8, 9],
+    attendace: 100,
+  },
+  {
+    id: 3,
+    name: 'Jerry',
+    age: 20,
+    grades: [8, 8, 10, 9, 10],
+    attendace: 120,
+  },
+];
+
+// let visitCount = new Map();
+// visitCount.set(students[0].attendace, 245);
+
+// console.log(visitCount.get(students[0].attendace));
