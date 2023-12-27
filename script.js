8945c51f43d6e6c0e2bf2f7be8cf4ab98ccfcd8b
elikemdaniels <hello@elikemdaniels.com>
2023-12-27 06:37:57 +0000

    #

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+/**
+ * Gets reference to page content div, creates new h1 element, sets text content
+ * of h1, and appends h1 to page content div.
+ */
+
+let pageContentChild = document.createElement('h1');
+pageContentChild.textContent = 'Learning Enough JavaScript to be Dangerous';
+
+let pageContent = document.getElementById('page-content');
+pageContent.appendChild(pageContentChild);
