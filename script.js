f23a7eca9bc63783321b99d7babcdbaff5c83628
elikemdaniels <hello@elikemdaniels.com>
2023-12-27 16:11:41 +0000

    #

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 // STRINGS
 
-let studentName = 'Emmanuel';
-let replacedName = studentName.replace('Emmanuel', 'John');
+// let studentName = 'Emmanuel';
+// let replacedName = studentName.replace('Emmanuel', 'John');
 
-console.log(studentName);
-console.log(replacedName);
-console.log(studentName.length === replacedName.length);
-console.log(studentName.slice(2, studentName.length).toUpperCase());
+// console.log(studentName);
+// console.log(replacedName);
+// console.log(studentName.length === replacedName.length);
+// console.log(studentName.slice(2, studentName.length).toUpperCase());
+
+// let userDetails = {
+//   username: 'olamide',
+//   email: 'olamide@gmail.com',
+//   password: '@87@092hbjhAHJ897!!@',
+//   isActive: true,
+// };
+
+// let prompter = prompt('What is your password?');
+
+// function passwordValidator(prompter) {
+//   if (prompter.length >= userDetails.password.length) {
+//     console.log('You are logged in');
+//   } else {
+//     console.log('Your password is too short');
+//   }
+// }
+
+// passwordValidator(prompter);
+
+// REGISTERED USERS LIST
+
+const registeredUsers = [
+  'olamide',
+  'emeka',
+  'akinboye',
+  'oluwaseun',
+  'oreogundipe',
+  'chiamaka',
+  'ikenna',
+];
