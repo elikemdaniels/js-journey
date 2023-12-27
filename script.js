340a8292348e35c9d1f78ff59ffea71683a0bd5a
elikemdaniels <hello@elikemdaniels.com>
2023-12-27 11:34:55 +0000

    String manipulated with the slice method

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // STRINGS
 
-let studentName = 'Jane';
-let replacedName = studentName.replace('Jane', 'John');
+let studentName = 'Emmanuel';
+let replacedName = studentName.replace('Emmanuel', 'John');
 
 console.log(studentName);
 console.log(replacedName);
 console.log(studentName.length === replacedName.length);
+console.log(studentName.slice(2, studentName.length));
