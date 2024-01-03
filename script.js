08559e396b0dad90d4e20e6e61a44f36ace205c6
elikemdaniels <hello@elikemdaniels.com>
2024-01-03 02:33:56 +0000

    #

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 // Select charactrs using id
 // const characters = document.querySelector('#star-wars-characters');
 
+// Array of characters
+const actors = ['James Bond', 'Sylvester Stallone', 'Steven Seagal'];
+const newCharacters = actors.map((actor) => {
+  const character = document.createElement('li');
+  character.textContent = actor;
+  return character;
+});
+
 // Select characters using tag
 const characters = document.querySelector('ul');
 
@@ -14,8 +22,27 @@ const characters = document.querySelector('ul');
 
 // Slelect character, Luke Skywalker using attribute
 const lukeSkywalker = characters.querySelector('[data-type="hero"]');
+const heading = document.querySelector('h1');
 const colourChangeButton = document.querySelector('#colour-change-button');
+const hideShowButton = document.querySelector('#hide-show-button');
+const defaultText = hideShowButton.textContent;
+const toggledText = 'Show Heading';
 
 colourChangeButton.addEventListener('click', () => {
   lukeSkywalker.classList.toggle('red');
+});
+
+function hideHeadingChangeButtonText() {
+  // Logic for toggling button text
+  if (hideShowButton.textContent === defaultText) {
+    hideShowButton.textContent = toggledText;
+  } else {
+    hideShowButton.textContent = defaultText;
+  }
+
+  heading.classList.toggle('hide');
+}
+
+hideShowButton.addEventListener('click', () => {
+  hideHeadingChangeButtonText();
 });
